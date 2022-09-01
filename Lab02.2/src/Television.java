014fa7bdee442639d60dd99f0a9230aa25197543
bluedestiny88 <bluedestiny88@gmail.com>
2022-09-01 15:42:23 -0700

    initial commit

---
+++ Lab02.2/src/Television.java
@@ -0,0 +1,18 @@
+public class Television {
+
+
+  String brand;
+  int volume;
+
+
+  public void turnOn() {
+    System.out.println("The " + brand + "is on, with a volume of " + volume);
+  }
+
+
+  public void turnOff() {
+    System.out.println("The " + brand + "is off, with a volume of " + volume);
+  }
+
+
+}
