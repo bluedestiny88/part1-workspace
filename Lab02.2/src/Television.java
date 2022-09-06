ba692b15ae18375b88304170b697ecfa0d4cf537
bluedestiny88 <bluedestiny88@gmail.com>
2022-09-06 09:17:30 -0700

    Adding some ignore files

--- Lab02.2/src/Television.java
+++ Lab02.2/src/Television.java
@@ -1,18 +1,33 @@
 public class Television {
 
-
-  String brand;
-  int volume;
-
+  private String brand;
+  private int volume;
 
   public void turnOn() {
     System.out.println("The " + brand + "is on, with a volume of " + volume);
   }
 
-
   public void turnOff() {
     System.out.println("The " + brand + "is off, with a volume of " + volume);
   }
 
+  public String getBrand() {
+    return brand;
+  }
 
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+  public int getVolume() {
+    return volume;
+  }
+
+  public void setVolume(int volume) {
+    this.volume = volume;
+  }
+
+  public String toString()  {
+    return "Television: brand = " + brand + ", volume = " + volume;
+  }
 }
--- Lab02.2/src/TelevisionClient.java
+++ Lab02.2/src/TelevisionClient.java
@@ -3,18 +3,21 @@ public class TelevisionClient {
   public static void main(String[] args)  {
 
     Television tv1 = new Television();
-    tv1.brand = "Zenith";
-    tv1.volume = 25;
+    tv1.setBrand("Samsung");
+    tv1.setVolume(32);
 
     Television tv2 = new Television();
-    tv2.brand = "RCA";
-    tv2.volume = 10;
+    tv2.setBrand("RCA");
+    tv2.setVolume(10);
 
     tv1.turnOn();
     tv1.turnOff();
 
     tv2.turnOn();
     tv2.turnOff();
+
+    System.out.println(tv1.toString());
+    System.out.println(tv1);
 
   }
 
